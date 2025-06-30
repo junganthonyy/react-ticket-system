63fe648e7669068a03889afe833878f3817cf2d3
junganthonyy <junganthonyy@gmail.com>
2025-06-30 16:36:40 -0700

    Adding DeleteBlock and TicketCard Components

---
+++ app/(components)/TicketCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DeleteBlock from './DeleteBlock'
+
+const TicketCard = () => {
+  return (
+    <div>
+      <DeleteBlock />
+    </div>
+  )
+}
+
+export default TicketCard
