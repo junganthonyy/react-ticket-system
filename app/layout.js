ef96ea4e59bfc004c4d8400e59dac365615b242a
junganthonyy <junganthonyy@gmail.com>
2025-06-30 16:31:14 -0700

    Adding Nav Stylings

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./(components)/Nav";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,8 +28,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Nav />
-        {children}
+        <div className="flex flex-col h-screen max-h-screen">
+          <Nav />
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
